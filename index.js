ec2d793eb1127f436a76f45d56fda02e42b7e948
Meow Roar <sareacct91@gmail.com>
2023-12-24 00:43:06 -0800

    Create a README file from user input

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 // TODO: Include packages needed for this application
+const { writeFile } = require("fs");
+const generateMarkdown = require("./assets/scripts/generateMarkdown");
+const getUserInput = require("./assets/scripts/getUserInput");
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(data, fileName = 'README.md') {
+  writeFile(`./generated/${fileName}`, generateMarkdown(data), err => err ? console.log(err) : console.log('README generated!'));
+}
 
 // TODO: Create a function to initialize app
-function init() {}
-
-// Function call to initialize app
-init();
+(async () => {
+  writeToFile( await getUserInput())
+})();

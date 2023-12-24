cf9e77fb9de1a1cba40fa8ebabdc240d15b5b039
Meow Roar <sareacct91@gmail.com>
2023-12-24 00:43:55 -0800

    Create a markdown string for the file

--- assets/scripts/generateMarkdown.js
+++ assets/scripts/generateMarkdown.js
@@ -1,18 +1,63 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(data) {
+  return `![license badge](https://img.shields.io/github/license/${data.githubName}/${data.githubRepoName})`
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(data) {
+  return `[${data.license}](https://github.com/${data.githubName}/${data.githubRepoName}/blob/master/LICENSE)`
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+const renderLicenseSection = (data) => data.license === 'No License' ? '' : `**${data.title}** is licensed under the ${renderLicenseLink(data)}.`;
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# ${data.title}    ${renderLicenseBadge(data)}
+
+## Description
+
+${data.description}
+
+## Table of Contents (Optional)
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+
+## Installation
+
+${data.installation}
+
+
+## Usage
+
+${data.usage}
+
+## License
+
+${renderLicenseSection(data)}
+
+## Contributing
+
+${data.contributing || `[Contributor Covenant](https://www.contributor-covenant.org/)`}
+
+## Tests
+
+${data.test}
+
+## Questions
+
+https://github.com/${data.githubName}
+
+If you have any questions email me at ${data.email}
 
 `;
 }
